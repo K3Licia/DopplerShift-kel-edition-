137c7f71a024b56b369a6638f04f3e6165577da7
K3L <135052649+K3Licia@users.noreply.github.com>
2026-05-20 08:16:09 -0500

    Add files via upload
    Signed-off-by: K3L <135052649+K3Licia@users.noreply.github.com>

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/dopplershift_preferences/luminescent.tsx
@@ -0,0 +1,7 @@
+import { FeatureChoiced } from '../base';
+import { FeatureDropdownInput } from '../dropdowns';
+
+export const luminescent_extract: FeatureChoiced = {
+  name: 'Extract',
+  component: FeatureDropdownInput,
+};
